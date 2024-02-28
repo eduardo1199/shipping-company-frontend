b8694d46604ae620fd52da14feeb4d529342b931
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2024-02-28 17:19:09 -0300

    feat: implement pagination in client page

--- src/pages/client/styles.ts
+++ src/pages/client/styles.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components'
-import { TableContainer as TableContainerMaterial } from '@mui/material'
+import {
+  Pagination,
+  TableContainer as TableContainerMaterial,
+} from '@mui/material'
 
 export const TableContainer = styled(TableContainerMaterial)`
   background: #e2e8f0;
@@ -9,4 +12,20 @@ export const TableContainer = styled(TableContainerMaterial)`
   border-radius: 1rem;
 
   margin-top: 2rem;
+`
+
+export const StyledPagination = styled(Pagination)`
+  button {
+    color: #111827;
+    background: #e2e8f0;
+
+    &.Mui-selected {
+      background: #1a202c;
+      color: #e2e8f0;
+    }
+  }
+
+  .MuiPaginationItem-ellipsis {
+    color: #e2e8f0;
+  }
 `
